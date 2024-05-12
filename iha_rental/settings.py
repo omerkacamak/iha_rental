7db30fec3f6b28958aa175528130d0be963e701f
Ömer Kaçamak <Omer.KACAMAK@opetfuchs.com.tr>
2024-05-12 17:59:45 +0300

    impelement postgresql

--- iha_rental/settings.py
+++ iha_rental/settings.py
@@ -96,10 +96,23 @@ WSGI_APPLICATION = 'iha_rental.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "ATOMIC_REQUESTS": True,
     }
 }
 
